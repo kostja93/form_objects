ea4c71c5d91c274937cf4ed5c6b24eace47df9c2
Konstantin Gontscharow <konstantin.gontscharow@zweitag.de>
2019-06-07 12:39:13 +0200

    add new form for employee created credit requests

--- app/controllers/employee_credit_requests_controller.rb
+++ app/controllers/employee_credit_requests_controller.rb
@@ -4,8 +4,24 @@ class EmployeeCreditRequestsController < ApplicationController
   end
 
   def new
+    @employee_credit_request = EmployeeCreditRequest.new
   end
 
   def create
+    @employee_credit_request = EmployeeCreditRequest.new(employee_credit_request_params)
+
+    if @employee_credit_request.save
+      flash[:notice] = 'Credit Request successfully created'
+      redirect_to employee_credit_requests_path
+    else
+      flash[:alert] = 'Something went wrong creating a Credit Request'
+      render :new
+    end
+  end
+
+  private
+
+  def employee_credit_request_params
+    params.require(:employee_credit_request).permit(EmployeeCreditRequest.new.attributes.keys)
   end
 end
